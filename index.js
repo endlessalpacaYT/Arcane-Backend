6ce21ca1c002d9bc04e6c165a571522ddf884693
ObsessedTech <90353497+endlessalpacaYT@users.noreply.github.com>
2023-12-10 23:24:00 +0000

    added config stuff and updated index slightly
    done for today cause i am too tired and started work pretty late

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from 'express';
-const express = require('express');
-const app = express();
+const http = require('express');
+const app = http();
+const mongoose = require("mongoose");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const rateLimit = require("express-rate-limit");
+const path = require("path");
 
+//paths that point to files so js can work with them
+const config = JSON.parse(fs.readFileSync("./Config/config.json").toString());
 
+//should create a http server with port "3551"
+//creating http server does not currently work
 const server = app.listen(3551);
