4bc82abf0178e1201dfb4e65ee4b3830069a0f4e
ObsessedTech <90353497+endlessalpacaYT@users.noreply.github.com>
2023-12-17 22:27:04 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require("mongoose")
+const fs = require("fs")
+const config = JSON.parse(fs.readFileSync("./config/config.json").toString())
 const app = express()
+const log = require("./structures/log.js");
 
 //should create a http server with port "3551"
 //creating http server does not currently work
 const port = 3551
 
+// lets the backend connect to the database
+mongoose.connect(config.mongodb.database, () => {
+    log.database("Arcane Backend has successfully connected to MongoDB.");
+});
+
 app.listen(port, () => {
-    console.log(`Arcane Backend listening on port ${port}`)
+    log.backend(`Arcane Backend listening on port ${port}`)
 })
