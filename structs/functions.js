ef973ee2d85cb1872b17d9acacd8b2af46b71c4b
ObsessedTech <90353497+endlessalpacaYT@users.noreply.github.com>
2024-01-02 16:46:44 +0000

    ngl i have no idea what i am doing xD

--- structs/functions.js
+++ structs/functions.js
@@ -4,6 +4,12 @@ const bcrypt = require("bcrypt");
 const fs = require("fs");
 const crypto = require("crypto");
 const path = require("path");
+
+async function sleep(ms) {
+    await new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
 function GetVersionInfo(req) {
     let memory = {
         season: 0,
